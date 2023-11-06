25fd95bf8ef71e22c0ea307ccf8e28d59c437bb0
Mirjigit <alaimirjigit154@gmail.com>
2023-11-06 17:22:40 +0600

    second commit

--- GeniusFlag/StartView.swift
+++ GeniusFlag/StartView.swift
@@ -9,23 +9,28 @@ import SwiftUI
 
 struct StartView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     @State private var countries = ["UK","USA","Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
     
-    @State private var showingScore = false
     @State private var scoreTitle = ""
     
     var body: some View {
         ZStack{
             LinearGradient(gradient: Gradient(colors: [.orange, .purple]), startPoint: .topTrailing, endPoint: .bottomLeading)
                 .edgesIgnoringSafeArea(.all)
-            VStack(spacing: 30){
-                VStack{
-                    HStack{
+            VStack(spacing: 50){
+                VStack()
+                {
+                    HStack(alignment: .center, spacing: 70){
+                        
                         Button{
+                            // Exit
+                            self.presentationMode.wrappedValue.dismiss()
                         } label: {
                             VStack{
                                 Image(systemName: "xmark")
@@ -38,7 +43,23 @@ struct StartView: View {
                         .background(.black.opacity(0.4))
                         .cornerRadius(10)
                         
-                        Button{
+                        Button{ // Timer
+                        } label: {
+                            ZStack{
+                                VStack{
+                                    Text("СЧЕТ")
+                                        .tint(.white)
+                                    Text("\(score)")
+                                        .tint(.white)
+                                        .font(.system(size: 25))
+                                }
+                            }
+                        }
+                        .frame(width: 60, height: 60)
+                        .background(.black.opacity(0.4))
+                        .cornerRadius(10)
+                        
+                        Button{ // Score
                         } label: {
                             ZStack{
                                 VStack{
@@ -69,7 +90,6 @@ struct StartView: View {
                 ForEach(0..<3) { number in
                     Button {
                         self.flagTapped(number)
-                        self.showingScore = true
                     } label: {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -80,18 +100,12 @@ struct StartView: View {
                                 .shadow(color: .black, radius: 4)
                     }
                 }
-                Text("Общий счет: \(score)")
+                /* Text("Общий счет: \(score)")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                
+                */
                 Spacer()
             }
-        }
-        
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message:  Text("Общий счет: \(score)"), dismissButton: .default(Text("Продолжить")){
-                self.askQuestion()
-            })
         }
     }
     func askQuestion(){
@@ -109,6 +123,7 @@ struct StartView: View {
             scoreTitle = "Неправильно! Это флаг \(countries[number])"
             score -= 1
         }
+        askQuestion()
     }
 }
 
